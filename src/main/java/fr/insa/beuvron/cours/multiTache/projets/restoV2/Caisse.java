1be325223c6194743a18d3fba222dcff8f0e6e65
baptiste-legold <baptiste.legold@gmail.com>
2022-12-02 14:03:59 +0100

    A vérifier

---
+++ src/main/java/fr/insa/beuvron/cours/multiTache/projets/restoV2/Caisse.java
@@ -0,0 +1,18 @@
+package fr.insa.beuvron.cours.multiTache.projets.restoV2;
+
+public class Caisse {
+    
+    private int id;
+    
+    public Caisse(int id) {
+        this.id = id;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
